1cfa001deca4c0c15c5d21a87c136884dd9cf9fb
Tine <tine@tjo.space>
2024-03-08 12:20:20 +0100

    fix: make postgresql accessible

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,9 +25,16 @@ resource "azurerm_postgresql_server" "main" {
   version    = "11"
   storage_mb = 5120
 
-  public_network_access_enabled    = true
+  public_network_access_enabled    = false
   ssl_enforcement_enabled          = true
   ssl_minimal_tls_version_enforced = "TLS1_2"
+}
+resource "azurerm_postgresql_firewall_rule" "main" {
+  name                = "azure-internal"
+  resource_group_name = azurerm_resource_group.main.name
+  server_name         = azurerm_postgresql_server.main.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
 }
 
 ##
